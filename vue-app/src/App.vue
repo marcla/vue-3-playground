b57a1bf16ea549002d83af388a7a693035dd5c7f
Claudio Marchiori <claudio.marchiori@codiceplastico.com>
2025-09-29 09:11:28 +0200

    feat: create new users's page

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import HomePage from "./components/HomePage.vue";
 import LoginPage from "./components/LoginPage.vue";
+import UsersPage from "./components/UsersPage.vue";
 
 export default {
   components: {
     HomePage,
     LoginPage,
+    UsersPage,
   },
   data: () => ({
     currentPage: "Home",
@@ -22,6 +24,9 @@ export default {
     showLoginPage() {
       this.currentPage = "Login";
     },
+    showUsersPage() {
+      this.currentPage = "Users";
+    },
   },
 };
 </script>
@@ -35,6 +40,7 @@ export default {
     <nav class="nav">
       <a href="#" @click.prevent="showHomePage">Home</a>
       <a href="#" @click.prevent="showLoginPage">Login</a>
+      <a href="#" @click.prevent="showUsersPage">Users</a>
     </nav>
   </header>
 
