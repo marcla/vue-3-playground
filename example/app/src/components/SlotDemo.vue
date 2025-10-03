d2a926133affc593382a8ccbb6b0130280bcb535
Claudio Marchiori <claudio.marchiori@codiceplastico.com>
2025-10-03 09:57:27 +0200

    feat: add defineExpose example

--- example/app/src/components/SlotDemo.vue
+++ example/app/src/components/SlotDemo.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref, useSlots } from 'vue';
 
 const currentCount = ref(99)
+const doubleCurrentCount = computed(() => currentCount.value * 2)
+const isOpen = ref(true);
+
+const toggleOpenState = () => {
+  console.log(`toggle inner open state`)
+  isOpen.value = !isOpen.value
+}
+const slots = useSlots()
+
+defineExpose({
+  currentCount,
+  doubleCurrentCount,
+  toggleOpenState
+})
+
 </script>
 
 <template>
-  <main>
+  <main class="slot-demo">
     <slot name="header">
       <h1>Slot title</h1>
     </slot>
+
+    <div>
+      <pre>isOpen: {{ isOpen }}</pre>
+    </div>
+
     <slot :count="currentCount">
       Current counter is: {{ currentCount }}
     </slot>
@@ -17,10 +37,18 @@ const currentCount = ref(99)
       <button @click="currentCount += 1">Incrementa</button>
     </p>
 
-    <slot name="footer">
+    <footer v-if="slots.footer">
+      <slot name="footer" />
+    </footer>
+    <!-- <slot name="footer">
       <p>This is the footer</p>
-    </slot>
+    </slot> -->
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.slot-demo {
+  padding: 6px 12px;
+  background-color: burlywood;
+}
+</style>
--- example/app/src/views/SandboxPage.vue
+++ example/app/src/views/SandboxPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, useTemplateRef } from 'vue';
 
 import BaseInputText from '@/components/BaseInputText.vue';
 import SlotDemo from '@/components/SlotDemo.vue';
@@ -7,24 +7,30 @@ import SlotDemo from '@/components/SlotDemo.vue';
 const fullname = ref('');
 const isLoading = ref(false);
 
+const slotDemoRef = useTemplateRef('slotDemo')
+
 </script>
 
 <template>
   <div class="container mx-auto p-8">
     <h1>Sandbox</h1>
 
-    <SlotDemo>
+    <pre>
+      slotDemo.doubleCurrentCount: {{ slotDemoRef?.currentCount }}
+      slotDemo.doubleCurrentCount: {{ slotDemoRef?.doubleCurrentCount }}
+    </pre>
+
+    <SlotDemo ref="slotDemo">
       <template v-slot:header>
         <h1>Titolo personalizzato</h1>
       </template>
 
       <template v-slot:default="{ count }">
         Contenuto personalizzato <b>{{ count }}</b>
-      </template>
 
-      <template v-slot:footer>
-        <BaseInputText class="w-full" label="Nome completo" v-model="fullname" placeholder="Enter task title..."
-          :disabled="isLoading" required />
+        <p>
+          <button @click="slotDemoRef?.toggleOpenState">Apri/Chiudi</button>
+        </p>
       </template>
     </SlotDemo>
 
